919ec24e44d3f3fab32d4712348b035412a0fbf8
Stefan Gojan <vollekannehoschi@gmail.com>
2013-11-09 19:58:21 +0100

    add setting form, without backend

--- app/scripts/controllers/overview.js
+++ app/scripts/controllers/overview.js
@@ -2,18 +2,33 @@
 
 angular.module('dashboardApp')
     .controller('OverviewCtrl', function($scope) {
-		$scope.projects = [
-			{
-				"name": "taginator",
-				"git": {
-					"dir": ".git"
-				},
-				"dir": "\/home\/foo\/repos\/taginator"
+	$scope.projects = [
+		{
+			"name": "taginator",
+			"git": {
+				"dir": ".git"
 			},
-			{
-				"name": "password-manager-test",
-				"dir": "\/home\/foo\/repos\/sweetp-code\/services\/password-manager\/sweetptest"
-			}
-		];
+			"dir": "\/home\/foo\/repos\/taginator"
+		},
+		{
+			"name": "password-manager-test",
+			"dir": "\/home\/foo\/repos\/sweetp-code\/services\/password-manager\/sweetptest"
+		}
+	];
+	// Settings
+
+	// TODO fetch from local storage
+	$scope.settings = {
+		serverUrl:"http://localhost:7777/"
+	};
+
+	// TODO save to local storage
+	$scope.saveSettings = function () {
+		if (!$scope.settingsForm.$valid) {
+			return;
+		}
+
+		console.log('save settings!!');
+	};
 });
 
