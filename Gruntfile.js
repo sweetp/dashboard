6eda23fc34ef5a31742927047906fa455ecc525c
Stefan Gojan <vollekannehoschi@gmail.com>
2013-12-29 09:02:54 +0100

    #3 ignore jasmine globals and lint also spec files with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,7 +130,8 @@ module.exports = function (grunt) {
       },
       all: [
         'Gruntfile.js',
-        '<%= yeoman.app %>/scripts/{,*/}*.js'
+        '<%= yeoman.app %>/scripts/{,*/}*.js',
+        'test/spec/{,*/}*.js'
       ]
     },
     coffee: {
