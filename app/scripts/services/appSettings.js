4180ac96caadc055b02a2d8908972c26918b07c4
Stefan Gojan <vollekannehoschi@gmail.com>
2013-12-29 09:02:54 +0100

    #3 use chrome storage API to save settings

--- app/scripts/services/appSettings.js
+++ app/scripts/services/appSettings.js
@@ -2,12 +2,21 @@
 
 angular.module('dashboardApp').factory('AppSettings', function() {
     return {
-        load:function () {
-            return {
-                serverUrl:"http://localhost:7777/"
-            };
+        load:function (callback) {
+            chrome.storage.local.get('app.settings', function (data) {
+                if (!data) {
+                    data = {
+                        serverUrl:"http://localhost:7777/"
+                    };
+                }
+
+                callback(null, data);
+            });
         },
-        save:function () {
+        save:function (settings, callback) {
+            chrome.storage.local.set({'app.settings': settings}, function () {
+                callback(null);
+            });
         }
     };
 });
--- test/spec/services/appSettings.js
+++ test/spec/services/appSettings.js
@@ -1,20 +1,85 @@
 'use strict';
 
 describe('Service: AppSettings', function () {
-  var s;
+	var s;
 
-  // load the controller's module
-  beforeEach(module('dashboardApp'));
+	// load the controller's module
+	beforeEach(module('dashboardApp'));
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (AppSettings) {
-      s = AppSettings;
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function (AppSettings) {
+        s = AppSettings;
+	}));
 
-  it('loads saved or default settings.', function () {
-    expect(s.load()).toEqual({
-		serverUrl:"http://localhost:7777/"
-    });
-  });
+    // mock chrome storage API
+    (function () {
+        /* jshint -W020 */
+        chrome = {
+            storage:{
+                local:{
+                    store:{},
+                    get:function (key, cb) {
+                        cb(this.store[key]);
+                    },
+                    set:function (store, cb) {
+                        this.store = store;
+                        cb();
+                    }
+                }
+            }
+        };
+    })();
+
+	it('loads saved or default settings.', function () {
+		var counter, loadedData;
+
+        // loads default
+        s.load(function (err, data) {
+            loadedData = data;
+        });
+
+        waitsFor(function () {
+            return loadedData !== undefined;
+        });
+
+        runs(function () {
+            expect(loadedData).toEqual({
+                serverUrl:"http://localhost:7777/"
+            });
+            loadedData = undefined;
+
+            // save something
+            counter = 0;
+            s.save({
+                serverUrl:"foo"
+            }, function (err) {
+                if (err) {
+                    throw new Error(err);
+                }
+                counter++;
+            });
+
+        });
+
+        runs(function () {
+            // callback of 'save' call was called
+            expect(counter).toBe(1);
+
+            // loads changed settings
+            s.load(function (err, data) {
+                loadedData = data;
+            });
+        });
+
+        waitsFor(function () {
+            return loadedData !== undefined;
+        });
+
+        runs(function () {
+            expect(loadedData).toEqual({
+                serverUrl:"foo"
+            });
+        });
+	});
 });
 
