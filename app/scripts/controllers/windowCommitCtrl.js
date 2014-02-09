f90339180e1c761cd59c376107677e54580949d6
Stefan Gojan <vollekannehoschi@gmail.com>
2014-02-09 17:24:19 +0100

    #13 add some directives for testing to commit window

--- app/scripts/controllers/windowCommitCtrl.js
+++ app/scripts/controllers/windowCommitCtrl.js
@@ -2,5 +2,5 @@
 
 angular.module('dashboardApp')
     .controller('WindowCommitCtrl', function($scope, $window) {
-		$scope.test = $window.sweetpWindowCommunication.project.name;
+		$scope.project = $window.sweetpWindowCommunication.project;
 });
