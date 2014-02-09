951d30e78675da7e01e6a25f5096a1327326f2b9
Stefan Gojan <vollekannehoschi@gmail.com>
2014-02-09 18:24:31 +0100

    #13 add directive to make a commit in the SCM

---
+++ app/scripts/directives/spScmCommitWidget.js
@@ -0,0 +1,94 @@
+'use strict';
+
+angular.module('dashboardApp')
+    .directive('spScmCommitWidget', function() {
+    return {
+        restrict: 'E',
+		scope: {
+			project: '='
+		},
+		controller: function ($scope, $log, Sweetp) {
+			var ctrl;
+
+			ctrl = this;
+			$scope.errors = null;
+			$scope.useAllFiles = 'true';
+			$scope.commitMessage = '';
+
+			this.handleServerError = function (err, data, status, scope) {
+				scope.errors = [
+					err,
+					"Service message: " + data.service
+				];
+				$log.error(err, data, status);
+			};
+
+			this.commitAgain = function (project, params, scope, callback) {
+				Sweetp.callService(project.name, 'scmenhancer/commit/again', params, function (err, data, status)  {
+					if (err) {
+						ctrl.handleServerError(err, data, status, scope);
+						return;
+					}
+
+					callback(null, data);
+				});
+			};
+
+			this.addAllFilesSwitch = function (params, useAllFiles) {
+				if (useAllFiles === "true") {
+					params.switches = ["-a"];
+				}
+			};
+
+			this.fixupCommit = function () {
+				var params;
+
+				// reset errors before new action
+				this.errors = null;
+
+				params = {
+					command:'fixup'
+				};
+				ctrl.addAllFilesSwitch(params, this.useAllFiles);
+
+				ctrl.commitAgain(this.project, params, this, function (err, data) {
+					if (err) {
+						$log.error(err);
+						return;
+					}
+
+					$log.info(data);
+					// TODO make notification?!
+					// TODO close window
+				});
+			};
+
+			this.commit = function () {
+				var params;
+
+				// reset errors before new action
+				this.errors = null;
+
+				params = {
+					message:this.commitMessage
+				};
+				ctrl.addAllFilesSwitch(params, this.useAllFiles);
+
+				Sweetp.callService(this.project.name, 'scm/commit', params, _.bind(function (err, data, status)  {
+					if (err) {
+						ctrl.handleServerError(err, data, status, this);
+						return;
+					}
+
+					$log.info(data);
+				}, this));
+			};
+
+			$scope.commit = this.commit;
+			$scope.fixupCommit = this.fixupCommit;
+		},
+        templateUrl: 'templates/spScmCommitWidget.html'
+    };
+});
+
+
